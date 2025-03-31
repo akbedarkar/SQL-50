2530e59f55ec64f02fe10a6b27658b17115d527f
Akshata Bedarkar <151378565+akbedarkar@users.noreply.github.com>
2025-03-31 13:34:46 +0530

    Update project-employees-i.sql

--- project-employees-i.sql
+++ project-employees-i.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
 
-select project_id,avg(experience_years) as average_years from Employee Inner Join  Project  on Employee.employee_id = Project.employee_id  Group By project_id
+select project_id,round(avg(experience_years),2) as average_years from Employee Inner Join  Project  on Employee.employee_id = Project.employee_id  Group By project_id
